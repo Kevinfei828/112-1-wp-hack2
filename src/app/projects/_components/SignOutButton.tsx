a97ec02ebb1401c22bf8b316d1c65e9f32d83cbb
Kevinfei828 <kevinfei828@gmail.com>
2023-12-20 17:17:55 +0800

    first commit

---
+++ src/app/projects/_components/SignOutButton.tsx
@@ -0,0 +1,15 @@
+// TODO: 4. Call the signOut() function when the button is clicked
+// hint: You may want to change the first line of this file
+'use client'
+import { signOut } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+import { publicEnv } from "@/lib/env/public";
+
+export default function SignOutButton() {
+  function handlesignout () {
+    signOut({callbackUrl: publicEnv.NEXT_PUBLIC_BASE_URL});
+  }
+
+  return <Button data-testid="sign-out-button" onClick={handlesignout} variant={"outline"}>Sign Out</Button>;
+}
+// TODO: 4. end
